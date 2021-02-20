1ff94f3aa5fe803455ba1cc2512a80e26254931b
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-20 18:11:45 +0100

    play ith Kwargs it's a square not rectangle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""square module for geometry lover"""
+
+
 class square():
     
     width = 0
@@ -7,13 +11,17 @@ class square():
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+            if 'width' in kwargs.keys() and self.height != self.width:
+                self.height = self.width
+            if 'height' in kwargs.keys() and self.width != self.height:
+                self.width = self.height
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return (self.width + self.height) * 2
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
@@ -22,5 +30,7 @@ if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
+    print(s.width)
+    print(s.height)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
