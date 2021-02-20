c28cd72b89515df77055578d653ae4b5a901c27c
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-20 19:58:22 +0100

    check again

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -22,11 +22,3 @@ class Square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
-
-
-if __name__ == "__main__":
-
-    s = Square(width=5, height=9)
-    print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
